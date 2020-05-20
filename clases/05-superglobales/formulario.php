1d6a5858aedc54344451667e0303afdbb7801819
MaggieSierra <43976301+MaggieSierra@users.noreply.github.com>
2020-05-20 09:14:31 -0500

    superglobales

---
+++ clases/05-superglobales/formulario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <form action="recibir.php" method="get">
+            <p> 
+                <label>Nombre:</label>
+                <input type="text" name="nombre">
+            </p>
+            <p> 
+                <label>Apellido:</label>
+                <input type="text" name="apellido">
+            </p>
+            <p>
+                <input type="submit" value="Enviar">
+            </p>
+        </form>
+    </body>
+</html>
+<?php
+
+?>
+
---
+++ clases/05-superglobales/recibir.php
@@ -0,0 +1,3 @@
+<?php
+echo '<h1>Bienvenido'." ".$_GET['nombre']." ".$_GET['apellido']."</h1>";
+?>
