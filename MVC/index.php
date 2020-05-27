d34bbe05c78fc902179975f845d1f1bb7fd5ba02
MaggieSierra <43976301+MaggieSierra@users.noreply.github.com>
2020-05-27 16:20:09 -0500

    index con opciones

--- MVC/index.php
+++ MVC/index.php
@@ -1,13 +1,25 @@
 <?php
 require_once 'controllers/usuario.php';
 $controlador = new UsuarioController();
-//$controlador->mostrarTodo();
-$controlador->crear();
 
-/* Tarea: Segmentar por funcionalidades el sitio respecto MVC
- * Objetos -> usuarios, ventas, cobranza, etc, etc, etc...
- * Modelos
- * Vistas
- * Controlador
- * Diseñar la base de datos (Seleccionar 2 tablas)
- */
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST["consulta"])) {
+    $controlador->mostrarTodo();
+} elseif ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST["crear"])) {
+    $controlador->crear();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuarios</title>
+</head>
+<body>
+    <form action="<?php $_SERVER["PHP_SELF"]; ?>" method="post">
+        <input type="submit" value="Mostrar usuarios" name="consulta">
+        <input type="submit" value="Crear usuario" name="crear">
+    </form>
+</body>
+</html>
